b06dfe0a9d833c778f8bf87b9d95b9e3b84d7eea
Evan Shaw <35987730+EvShaw@users.noreply.github.com>
2024-05-19 12:47:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,8 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ', '.join(ingredients_list)
+    # ingredients_string = ', '.join(ingredients_list)
+    ingredients_string = ''
 
     # insert_stmt = f"""
     # INSERT INTO smoothies.public.orders (ingredients, name_on_order)
@@ -71,8 +72,9 @@ if ingredients_list:
     # """
 
     for fruit_chosen in ingredients_list:
+         ingredients_string += fruit chosen + ' '
         
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(f'{fruit_chosen} Nutrition Information')
